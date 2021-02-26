47808bd2f0f4081e79f9a372f628ab49e21b41fd
CluckFox <cjpizzi@outlook.com>
2021-02-26 02:19:04 -0500

    class to hold transformation parameters

---
+++ BitmapManipulator/Resizer.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace tiled_transporter
+{
+    namespace BitmapManipulator
+    {
+
+        /// <summary>Class <c>Resizer</c> represents parameters for a
+        /// tile-to-tile transformation.</summary>
+        class Resizer
+        {
+            public enum ResizeOps : byte
+            {
+                NOP = 0x00,
+                HFLIP = 0x01,
+                VFLIP = 0x02,
+                R1ROT = 0x04,
+                R2ROT = 0x08
+            }
+            ResizeOps ops = ResizeOps.NOP;
+            byte targetW = 0x10;
+            byte targetH = 0x10;
+            public Resizer()
+            {
+
+            }
+            public Resizer(byte targetW, byte targetH, ResizeOps ops)
+            {
+                this.targetW = targetW;
+                this.targetH = targetH;
+                this.ops = ops;
+            }
+
+
+
+            public byte TargetH { get => targetH; set => targetH = value; }
+            public byte TargetW { get => targetW; set => targetW = value; }
+            internal ResizeOps Ops { get => ops; set => ops = value; }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Resizer resizer &&
+                       ops == resizer.ops &&
+                       targetW == resizer.targetW &&
+                       targetH == resizer.targetH;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(ops, targetW, targetH);
+            }
+        }
+
+    }
+}
