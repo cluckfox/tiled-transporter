8a37b576127a24eddf5d8bf69bd2d14cc440a0f9
CluckFox <cjpizzi@outlook.com>
2021-02-27 13:38:58 -0500

    tileset_spec for per-tileset data to load

---
+++ BitmapManipulator/TilesetLoader/tileset_spec.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace tiled_transporter {
+    namespace TilesetLoader {
+        struct tileset_spec {
+            char[] namespec;
+
+            public tileset_spec(char[] namespec, ushort countspec) : this()
+            {
+                this.namespec = namespec;
+                this.countspec = countspec;
+            }
+
+            ushort countspec;
+
+            public char[] Namespec { get => namespec; set => namespec = value; }
+            public ushort Countspec { get => countspec; set => countspec = value; }
+
+            public override bool Equals(object obj)
+            {
+                return obj is tileset_spec spec &&
+                       EqualityComparer<char[]>.Default.Equals(namespec, spec.namespec) &&
+                       countspec == spec.countspec;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(namespec, countspec);
+            }
+        }
+    }
+}
