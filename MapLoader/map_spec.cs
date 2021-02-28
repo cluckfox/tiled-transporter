4e0a95d5ac11bab9df90a445c70a0ec6b5e90c77
CluckFox <cjpizzi@outlook.com>
2021-02-28 11:34:45 -0500

    add field for CSV CDATA

--- MapLoader/map_spec.cs
+++ MapLoader/map_spec.cs
@@ -10,17 +10,20 @@ namespace tiled_transporter
           char[] namespec;
           char[] tilesetspec;
           UInt32 firstgidspec;
+          char[] cdataspec;
 
-            public map_spec(char[] namespec, char[] tilesetspec, uint firstgidspec)
+            public map_spec(char[] namespec, char[] tilesetspec, uint firstgidspec, char[] cdataspec)
             {
                 this.namespec = namespec;
                 this.tilesetspec = tilesetspec;
                 this.firstgidspec = firstgidspec;
+                this.cdataspec = cdataspec;
             }
 
             public char[] Namespec { get => namespec; set => namespec = value; }
             public char[] Tilesetspec { get => tilesetspec; set => tilesetspec = value; }
             public uint Firstgidspec { get => firstgidspec; set => firstgidspec = value; }
+            public char[] Cdataspec { get => cdataspec; set => cdataspec = value; }
 
             public override bool Equals(object obj)
             {
@@ -31,12 +34,13 @@ namespace tiled_transporter
             {
                 return EqualityComparer<char[]>.Default.Equals(namespec, other.namespec) &&
                        EqualityComparer<char[]>.Default.Equals(tilesetspec, other.tilesetspec) &&
-                       firstgidspec == other.firstgidspec;
+                       firstgidspec == other.firstgidspec &&
+                       EqualityComparer<char[]>.Default.Equals(cdataspec, other.cdataspec);
             }
 
             public override int GetHashCode()
             {
-                return HashCode.Combine(namespec, tilesetspec, firstgidspec);
+                return HashCode.Combine(namespec, tilesetspec, firstgidspec, cdataspec);
             }
 
             public static bool operator ==(map_spec left, map_spec right)
